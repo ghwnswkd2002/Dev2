37487971adbb080a49dbad9cf730607dba61f1c3
HoJun <ghwnswkd2002@naver.com>
2019-04-20 17:06:03 +0900

    시작

---
+++ src/main/java/dev2/springboot/groupware/GroupwareApplication.java
@@ -0,0 +1,13 @@
+package dev2.springboot.groupware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupwareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupwareApplication.class, args);
+    }
+
+}
